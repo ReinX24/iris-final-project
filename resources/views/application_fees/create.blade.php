f3c03d62ec3f567b054c8435819a1c49588f98ff
Rein Solis <reinX244@gmail.com>
2025-07-17 13:19:35 +0900

    Final demo files added.

--- resources/views/application_fees/create.blade.php
+++ resources/views/application_fees/create.blade.php
@@ -139,7 +139,7 @@
                         <!-- Action Buttons -->
                         <div class="mt-8 flex justify-end space-x-4">
                             <!-- Back Button -->
-                            <a href="{{ route('application-fees.index') }}"
+                            <a href="{{ route('applicants.show', $selectedApplicant->id) }}"
                                 class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
                                 <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                                     xmlns="http://www.w3.org/2000/svg">
